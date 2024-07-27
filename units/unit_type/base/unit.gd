44556e18a7502f704de1485e924a2aa677da9c09
bysmor <bysmorian@gmail.com>
2024-07-26 21:27:57 -0500

    Movement tweaks, line drawn for movement, drawing tweaks

--- units/unit_type/base/unit.gd
+++ units/unit_type/base/unit.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 class_name base_unit
 
 const SPEED = 300.0
-var TARGET_POSITION: Vector2
+@onready var TARGET_POSITION: Vector2 = self.position
 
 func _physics_process(delta):
-	_unit_move_toward_point(TARGET_POSITION)
+	_unit_move_toward_point()
 	move_and_slide()
 
-func _unit_move_toward_point(target_position):
-	var direction = position.direction_to(target_position)
+func _unit_move_toward_point():
+	var direction = position.direction_to(TARGET_POSITION)
 	
-	if direction:
+	if direction && (self.position.distance_to(TARGET_POSITION) > 5):
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
