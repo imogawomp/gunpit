786e90391f866d314c10220f0485f11e2dd27c4a
Coleman White <cwhite69@uco.edu>
2024-07-27 17:08:25 -0500

    garbage4

--- units/unit_type_friendly/riot/unit_riot_turret/unit_riot_turret.gd
+++ units/unit_type_friendly/riot/unit_riot_turret/unit_riot_turret.gd
@@ -6,38 +6,33 @@ const friendly_unit_riot = preload("res://units/unit_type_friendly/riot/unit_rio
 @onready var TURRET_POSITION = self.position
 @onready var TARGET_BODIES: Array = []
 
+
 func _process(delta):
 	look_at(TURRET_POSITION)
 	_fire_projectile()
 
 func _fire_projectile():
+	queue_redraw()
 	for body in TARGET_BODIES:
-		print(body)
+		#var unit_projectile = friendly_unit_riot.instantiate()
+		pass
 
-func _on_body_shape_entered(body):
-	if !TARGET_BODIES.has(body):
+func _on_body_shape_entered(rid_body: RID, body: Node2D, idx: int, loc_idx: int):
+	if !TARGET_BODIES.has(body) && body.is_in_group("enemy"):
 		TARGET_BODIES.push_back(body)
+
 	
-func _on_body_shape_exited(body):
+func _on_body_shape_exited(rid_body: RID, body: Node2D, idx: int, loc_idx: int):
 	if TARGET_BODIES.has(body):
 		var indx = TARGET_BODIES.find(body)
 		TARGET_BODIES.remove_at(indx)
-	
-	#while :
-		#var tempx = global_position.x + randf_range(0, 500)
-		#var tempy = global_position.y + randf_range(0, 500)
-		#
-		#if friendly_posx_array && friendly_posy_array:
-			#for x in friendly_posx_array.size():
-				#while (tempx >= friendly_posx_array[x] + 100 && tempx < friendly_posx_array[x] + 100):
-					#tempx = global_position.x + randf_range(0, 250);
-				#while (tempy >= friendly_posy_array[x] + 100 && tempy < friendly_posy_array[x] + 100):
-					#tempy = global_position.y + randf_range(0, 250);
-				#
-		#var f_unit_riot = friendly_unit_riot.instantiate()
-		#f_unit_riot.global_position = Vector2(tempx, tempy)
-		#
-		#friendly_posx_array.push_back(tempx)
-		#friendly_posy_array.push_back(tempy)
-		#
-		#self.add_child(f_unit_riot)
+
+func _draw():
+	for body in TARGET_BODIES:
+		draw_line(Vector2(0,0), Vector2(self.global_position.distance_to(body.global_position), 0), Color(0,1,0,0.25), 3)
+
+			
+	#var f_unit_riot = friendly_unit_riot.instantiate()
+	#f_unit_riot.global_position = Vector2(tempx, tempy)
+	#
+	#self.add_child(f_unit_riot)
